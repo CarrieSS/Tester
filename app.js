45b21ea5574ab7d472ca662069c3ce34ff1cc460
CarrieSS <carrie.shenbca@gmail.com>
2016-08-12 11:42:38 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var app = angular.module("myApp", [])
+		.controller("myCtrl", function ($scope, $http) { 
+
+			var companyCode;
+
+			$scope.isAuthenticated = false;
+
+			$scope.login = function(code){
+				if(code != null){
+					$http.get('default.json').then((response) => {
+						$scope.message = response.data;
+					})
+					$scope.isAuthenticated = true;	
+				
+					companyCode = code;
+					return companyCode;
+					
+				} else{
+					$scope.errorMessage = {
+						error: "Invalid company code."
+					};
+				}
+			};
+
+			$scope.saveConfirm = function(){
+
+				var inputs = document.getElementById('Menu').getElementsByTagName('input');
+				var obj = Object();
+				var objFr = Object();
+
+				for(var i = 0; i < inputs.length; i++){
+					obj[inputs[i].id] = inputs[i].value;
+					objFr[inputs[i].id] = inputs[i].value + "FRENCH";
+				}
+
+				console.log(JSON.stringify(obj));
+				console.log(JSON.stringify(objFr));
+	
+				console.log(companyCode);
+
+			};
+		});
